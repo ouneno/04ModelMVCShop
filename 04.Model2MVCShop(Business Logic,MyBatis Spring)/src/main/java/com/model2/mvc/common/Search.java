7a128425734dab3cad9d686118e3880196d73123
gkstj <gkstj@DESKTOP-46JA1TO>
2021-06-01 20:39:07 +0900

    commit
    - 04.Model2MVCShop END

--- 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/main/java/com/model2/mvc/common/Search.java
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/main/java/com/model2/mvc/common/Search.java
@@ -8,7 +8,7 @@ public class Search {
 	private int currentPage;
 	private String searchCondition;
 	private String searchKeyword;
-	private int pageSize;
+	private int pageSize; 
 	//==> 리스트화면 currentPage에 해당하는 회원정보를 ROWNUM 사용 SELECT 위해 추가된 Field 
 	//==> UserMapper.xml 의 
 	//==> <select  id="getUserList"  parameterType="search"	resultMap="userSelectMap">
